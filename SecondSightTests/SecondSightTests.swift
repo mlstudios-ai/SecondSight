0580b19504ef85bb8a64704ed41ae62db982e16f
Anna Huang <chilliavocado@pm.me>
2025-03-24 23:04:08 +1100

    Initial Commit

---
+++ SecondSightTests/SecondSightTests.swift
@@ -0,0 +1,17 @@
+//
+//  SecondSightTests.swift
+//  SecondSightTests
+//
+//  Created by Jasper on 24/3/2025.
+//
+
+import Testing
+@testable import SecondSight
+
+struct SecondSightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
